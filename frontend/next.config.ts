2cf84092871ef44690487c12dd0b67613686f578
energicom-hue <energicom@gmail.com>
2026-04-25 10:18:03 -0500

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,3 +1,9 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {};
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
